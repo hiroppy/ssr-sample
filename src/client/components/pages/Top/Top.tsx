d44956327a4f4f1f02bc8bb2d6e9a8b90df19adf
Yuta Hiroto <git@about-hiroppy.com>
2018-09-01 18:45:27 +0900

    refactor(Top): change import order

--- src/client/components/pages/Top/Top.tsx
+++ src/client/components/pages/Top/Top.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { Head } from '../../Head';
 import styled from 'styled-components';
+import { Head } from '../../Head';
 
 export interface Props {
   load: () => void;
