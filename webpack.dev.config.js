c857123f3976d9f4b02f92288cb1d2763743623e
Yuta Hiroto <git@about-hiroppy.com>
2018-10-12 21:38:37 +0900

    chore: modify webpack.config and tsconfig

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -5,6 +5,7 @@ const webpack = require('webpack');
 
 const config = {
   entry: ['webpack-hot-middleware/client', resolve('src', 'client', 'index.tsx')],
+  devtool: 'inline-cheap-module-source-map',
   plugins: [new webpack.HotModuleReplacementPlugin()]
 };
 
