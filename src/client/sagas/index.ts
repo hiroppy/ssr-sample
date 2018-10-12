71605378f086fa62535e9193b2bcb8d453eae3d9
Yuta Hiroto <git@about-hiroppy.com>
2018-10-12 21:39:11 +0900

    refactor(saga): change order

--- src/client/sagas/index.ts
+++ src/client/sagas/index.ts
@@ -1,10 +1,10 @@
 import { all, fork } from 'redux-saga/effects';
-import { orgsProcess } from './orgs';
 import { pagesProcess } from './pages';
+import { orgsProcess } from './orgs';
 
 /**
  * Root for saga
  */
 export function* rootSaga() {
-  yield all([fork(orgsProcess), fork(pagesProcess)]);
+  yield all([fork(pagesProcess), fork(orgsProcess)]);
 }
