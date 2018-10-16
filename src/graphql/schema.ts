cf2c2a7d6650b81b7d03e7428daf49e0ce64aac2
Yuta Hiroto <git@about-hiroppy.com>
2018-10-17 08:06:04 +0900

    feat(graphql): add apollo-client, resolvers and executableSchema

---
+++ src/graphql/schema.ts
@@ -0,0 +1,38 @@
+import { makeExecutableSchema } from 'graphql-tools';
+
+export const organizations = [
+  {
+    name: 'nodejs',
+    uri: 'https://github.com/nodejs',
+    uid: 1
+  },
+  {
+    name: 'facebook',
+    uri: 'https://github.com/facebook',
+    uid: 2
+  },
+  {
+    name: 'google',
+    uri: 'https://github.com/google',
+    uid: 3
+  },
+  {
+    name: 'microsoft',
+    uri: 'https://github.com/microsoft',
+    uid: 4
+  }
+];
+
+export const typeDefs = `
+  type Query { organizations: [Organization] }
+  type Organization { name: String, uri: String, uid: Int }
+`;
+
+export const resolvers = {
+  Query: { organizations: () => organizations }
+};
+
+export const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
