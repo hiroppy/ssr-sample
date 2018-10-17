67cdd915b1dce0083c4f6b10deedf34a2e1a4903
Yuta Hiroto <git@about-hiroppy.com>
2018-10-17 19:48:18 +0900

    chore(storybook): refactor config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,24 +1,6 @@
 'use strict';
 
-const path = require('path');
-const webpack = require('webpack');
+const merge = require('webpack-merge');
+const base = require('../webpack.config');
 
-module.exports = (baseConfig, env, config) => {
-  const rules = [
-    {
-      // for graphql
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: 'javascript/auto'
-    },
-    {
-      test: /\.(ts|tsx)$/,
-      use: require.resolve('awesome-typescript-loader')
-    }
-  ];
-
-  config.module.rules.push(...rules);
-  config.resolve.extensions.push('.ts', '.tsx');
-
-  return config;
-};
+module.exports = (baseConfig, env, config) => merge.smart(base, config);
