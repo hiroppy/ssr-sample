295f6210a4ad57a6f946e00d487c4743c031840b
Yuta Hiroto <git@about-hiroppy.com>
2018-10-23 11:35:50 +0900

    test(Top): use mock

--- src/client/components/pages/Top/Top.test.tsx
+++ src/client/components/pages/Top/Top.test.tsx
@@ -3,12 +3,48 @@ import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import * as sinon from 'sinon';
 import { MemoryRouter } from 'react-router-dom';
+import { gql } from 'apollo-boost';
 import { MockedProvider } from 'react-apollo/test-utils';
 import { Top } from '.';
 
+const mockedData = {
+  organizations: [
+    {
+      uid: 1,
+      name: 'name',
+      uri: 'uri'
+    },
+    {
+      uid: 2,
+      name: 'name2',
+      uri: 'uri'
+    },
+    {
+      uid: 3,
+      name: 'name3',
+      uri: 'uri'
+    },
+    {
+      uid: 4,
+      name: 'name4',
+      uri: 'uri'
+    }
+  ]
+};
+
 test('should render self and sub-components', () => {
+  const GET_ORGS = gql`
+    {
+      organizations {
+        name
+        uid
+      }
+    }
+  `;
+  const mocks = [{ request: { query: GET_ORGS }, result: { data: mockedData } }];
+
   const tree = shallow(
-    <MockedProvider>
+    <MockedProvider mocks={mocks} addTypename={false}>
       <MemoryRouter initialEntries={[{ pathname: '/', key: 'testKey' }]}>
         <Top error={null} load={() => {}} />
       </MemoryRouter>
