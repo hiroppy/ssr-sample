788e370f2e98114f49dcfba8f49b5e8518ae39a6
Yuta Hiroto <git@about-hiroppy.com>
2018-10-26 18:54:55 +0900

    chore(package): add @storybook/addon-knobs and @storybook/addon-viewport

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,10 @@
 import * as React from 'react';
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import viewport from '@storybook/addon-viewport';
 
 const req = require.context('../src/client', true, /.stories.tsx$/);
+
+addDecorator([viewport]);
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename));
