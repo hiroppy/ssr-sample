259ff5a37e5005c5a6e9ee44306c4449815d7de9
Chris <jovanovski.kristijan.2@students.finki.ukim.mk>
2018-12-09 20:58:56 +0100

    revert some file changes

--- src/client/store/configureStore.ts
+++ src/client/store/configureStore.ts
@@ -12,8 +12,8 @@ const createEnhancer = () => {
   const composeEnhancers =
     process.env.NODE_ENV !== 'production' &&
     typeof window === 'object' &&
-    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-      ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
       : compose;
 
   return composeEnhancers(applyMiddleware(sagaMiddleware, routerMiddleware(history)));
