f7209d88a14e14eeed25e976361891d04927dbd9
Yuta Hiroto <git@about-hiroppy.com>
2019-05-02 05:58:32 +0100

    chore(babelrc, webpack): make use of babel-loader

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,19 @@
 module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage' // experimental
+      }
+    ],
+    '@babel/preset-typescript',
+    '@babel/preset-react'
+  ],
+  plugins: [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread'
+  ],
   env: {
     test: {
       plugins: ['macros']
