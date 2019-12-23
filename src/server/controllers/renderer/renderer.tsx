b08766784dd249f3a8abe1079de4694ede021d46
Youssef <youss.mesini@gmail.com>
2019-12-23 16:24:45 +0100

    feat(stream): trying to use renderToNodeStream

--- src/server/controllers/renderer/renderer.tsx
+++ src/server/controllers/renderer/renderer.tsx
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 import { Request, Response } from 'express';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { renderToString, renderToStaticMarkup } from 'react-dom/server';
+import { renderToString, renderToStaticMarkup, renderToNodeStream } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import Helmet from 'react-helmet';
 import { ServerStyleSheet } from 'styled-components';
@@ -15,6 +15,15 @@ import { configureStore } from '../../../client/store/configureStore';
 import { createClient } from '../../../graphql/client';
 import { schema } from '../../../graphql/schema';
 import { setBaseUrl } from '../../../client/actions/pages';
+
+const escape = (str: string) => {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+};
 
 const statsFile = resolve(
   __dirname,
@@ -39,41 +48,52 @@ export async function get(req: Request, res: Response) {
       <Provider store={store}>
         <StaticRouter location={req.url} context={context}>
           {/* add `div` because of `hydrate` */}
-          <div id="root">
-            <Router />
-          </div>
+          <Router />
         </StaticRouter>
       </Provider>
     </ApolloProvider>
   );
+  runSaga();
 
-  try {
-    const extractor = new ChunkExtractor({ statsFile });
-    const tree = extractor.collectChunks(<App />);
+  const preloadedState = JSON.stringify(store.getState());
+  const graphql = JSON.stringify(client.extract());
 
-    await Promise.all([
-      // kick apollo, redux-saga, and styled-components
-      getMarkupFromTree({
-        tree,
-        renderFunction: renderToStaticMarkup
-      }),
-      runSaga()
-    ]);
-
-    const body = renderToString(tree);
-    const preloadedState = JSON.stringify(store.getState());
-    const helmetContent = Helmet.renderStatic();
-    const meta = `
+  const extractor = new ChunkExtractor({ statsFile });
+  const tree = extractor.collectChunks(<App />);
+  const scripts = extractor.getScriptTags({ nonce });
+  const helmetContent = Helmet.renderStatic();
+  const meta = `
       ${helmetContent.meta.toString()}
       ${helmetContent.title.toString()}
     `.trim();
-    const style = sheet.getStyleTags();
-    const scripts = extractor.getScriptTags({ nonce });
-    const graphql = JSON.stringify(client.extract());
+  await Promise.all([
+    // kick apollo, redux-saga, and styled-components
+    getMarkupFromTree({
+      tree,
+      renderFunction: renderToStaticMarkup
+    }),
+    runSaga()
+  ]);
+  res.write(`<!DOCTYPE html><html lang="en"><head>
+    <meta charSet="UTF-8" />
+    <meta name="Description" content="introducing SPA and SSR">
+    <meta property="csp-nonce" content="${nonce}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    ${meta}
+    </head><body><script nonce="${nonce}" id="initial-data" type="text/plain" data-json="${escape(
+    preloadedState
+  )}"></script>
+  <script nonce="${nonce}">window.__APOLLO_STATE__=${graphql}</script>
+  ${scripts}
 
-    return res.send(renderFullPage({ meta, body, style, preloadedState, scripts, graphql, nonce }));
-  } catch (e) {
-    console.error(e);
-    return res.status(500).send(e.message);
-  }
+  `);
+  res.write("<div id='root'>");
+  const jsx = sheet.collectStyles(tree);
+
+  const stream = sheet.interleaveWithNodeStream(renderToNodeStream(jsx));
+  stream.pipe(res, { end: false });
+  stream.on('end', () => {
+    res.write('</body></html>');
+    res.end();
+  });
 }
