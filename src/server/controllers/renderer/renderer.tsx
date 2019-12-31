8d99085e30ff4390ee6251cc00c6b05e7e90caa9
Youssef Meskini <youss.meskini@gmail.com>
2019-12-31 08:33:41 +0100

    feat(stream): adapting the rendering

--- src/server/controllers/renderer/renderer.tsx
+++ src/server/controllers/renderer/renderer.tsx
@@ -2,14 +2,13 @@ import { resolve } from 'path';
 import { Request, Response } from 'express';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { renderToString, renderToStaticMarkup, renderToNodeStream } from 'react-dom/server';
+import { renderToStaticMarkup, renderToNodeStream } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import Helmet from 'react-helmet';
 import { ServerStyleSheet } from 'styled-components';
 import { ChunkExtractor } from '@loadable/server';
 import { ApolloProvider, getMarkupFromTree } from 'react-apollo';
 import { SchemaLink } from 'apollo-link-schema';
-import { renderFullPage } from '../../renderFullPage';
 import { Router } from '../../../client/router/index';
 import { configureStore } from '../../../client/store/configureStore';
 import { createClient } from '../../../graphql/client';
@@ -48,7 +47,9 @@ export async function get(req: Request, res: Response) {
       <Provider store={store}>
         <StaticRouter location={req.url} context={context}>
           {/* add `div` because of `hydrate` */}
-          <Router />
+          <div id="root">
+            <Router />
+          </div>
         </StaticRouter>
       </Provider>
     </ApolloProvider>
@@ -56,11 +57,11 @@ export async function get(req: Request, res: Response) {
   runSaga();
 
   const preloadedState = JSON.stringify(store.getState());
+  const initialState = escape(preloadedState);
   const graphql = JSON.stringify(client.extract());
 
   const extractor = new ChunkExtractor({ statsFile });
   const tree = extractor.collectChunks(<App />);
-  const scripts = extractor.getScriptTags({ nonce });
   const helmetContent = Helmet.renderStatic();
   const meta = `
       ${helmetContent.meta.toString()}
@@ -74,26 +75,31 @@ export async function get(req: Request, res: Response) {
     }),
     runSaga()
   ]);
-  res.write(`<!DOCTYPE html><html lang="en"><head>
-    <meta charSet="UTF-8" />
-    <meta name="Description" content="introducing SPA and SSR">
-    <meta property="csp-nonce" content="${nonce}">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    ${meta}
-    </head><body><script nonce="${nonce}" id="initial-data" type="text/plain" data-json="${escape(
-    preloadedState
-  )}"></script>
-  <script nonce="${nonce}">window.__APOLLO_STATE__=${graphql}</script>
-  ${scripts}
 
-  `);
-  res.write("<div id='root'>");
+  res.write(
+    `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="Description" content="introducing SPA and SSR">
+        <meta property="csp-nonce" content="${nonce}">
+        ${meta}
+        <link rel="manifest" href="/manifest.webmanifest">
+        <link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet"></link>
+      </head>
+      <body>
+        <script nonce="${nonce}" id="initial-data" type="text/plain" data-json="${initialState}"></script>
+        <script nonce="${nonce}">window.__APOLLO_STATE__=${graphql}</script>
+  `.trim()
+  );
+
   const jsx = sheet.collectStyles(tree);
-
   const stream = sheet.interleaveWithNodeStream(renderToNodeStream(jsx));
   stream.pipe(res, { end: false });
   stream.on('end', () => {
-    res.write('</body></html>');
+    res.write(`
+    ${extractor.getScriptTags({ nonce })}</body></html>`);
     res.end();
   });
 }
--- src/server/renderFullPage.ts
+++ src/server/renderFullPage.ts
@@ -1,11 +1,14 @@
+import { Response } from 'express';
+
 type Params = {
   meta: string;
-  body: string;
-  style: string;
+  body?: string;
+  style?: string;
   preloadedState: string;
   graphql: string;
   nonce: string;
   scripts: string;
+  res: Response;
 };
 
 const escape = (str: string) => {
@@ -24,28 +27,30 @@ export const renderFullPage = ({
   scripts,
   preloadedState,
   graphql,
-  nonce
+  nonce,
+  res
 }: Params) => {
-  return `<!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="Description" content="introducing SPA and SSR">
-        <meta property="csp-nonce" content="${nonce}">
-        ${meta}
-        <link rel="manifest" href="/manifest.webmanifest">
-        <link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet"></link>
-        ${style}
-      </head>
-      <body>
-        ${body}
-        <script nonce="${nonce}" id="initial-data" type="text/plain" data-json="${escape(
-    preloadedState
-  )}"></script>
-        <script nonce="${nonce}">window.__APOLLO_STATE__=${graphql}</script>
-        ${scripts}
-      </body>
-    </html>
-  `.trim();
+  // TODO
+  res.write(
+    `<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="Description" content="introducing SPA and SSR">
+          <meta property="csp-nonce" content="${nonce}">
+          ${meta}
+          <link rel="manifest" href="/manifest.webmanifest">
+          <link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet"></link>
+          ${style}
+        </head>
+        <body>
+          ${body}
+          <script nonce="${nonce}" id="initial-data" type="text/plain" data-json="${escape(
+      preloadedState
+    )}"></script>
+          <script nonce="${nonce}">window.__APOLLO_STATE__=${graphql}</script>
+          ${scripts}
+    `.trim()
+  );
 };
